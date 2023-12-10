21523aea193821c88ce5b9ca5753d86096041fb4
optimisme <optimisme@gmail.com>
2023-12-10 11:31:32 +0100

    .

--- editor_base/lib/layout_sidebar_tools.dart
+++ editor_base/lib/layout_sidebar_tools.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cupertino_desktop_kit/cdk.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'app_data.dart';
 import 'util_button_icon.dart';
 
@@ -12,50 +13,33 @@ class LayoutSidebarTools extends StatelessWidget {
     AppData appData = Provider.of<AppData>(context);
     CDKTheme theme = CDKThemeNotifier.of(context)!.changeNotifier;
 
-    Color colorSelected = theme.colorText;
-    Color colorUnselected = theme.isLight
-        ? const Color.fromARGB(255, 15, 15, 15)
-        : const Color.fromARGB(255, 250, 0, 0);
+    List<String> tools = [
+      "pointer_shapes",
+      "shape_drawing",
+    ];
 
-    return Column(children: [
-      Container(
-        padding: const EdgeInsets.only(top: 2, left: 2),
-        child: UtilButtonIcon(
-          size: 24,
-          isSelected: appData.toolSelected == "pointer_shapes",
-          onPressed: () {
-            appData.setToolSelected("pointer_shapes");
-          },
-          child: Opacity(
-            opacity: appData.toolSelected == "pointer_shapes" ? 1.0 : 0.5,
-            child: Image.asset('assets/images/pointer_shapes.png',
-                color: appData.toolSelected == "pointer_shapes"
-                    ? colorSelected
-                    : colorUnselected,
-                width: 18,
-                height: 18),
-          ),
-        ),
-      ),
-      Container(
-        padding: const EdgeInsets.only(top: 2, left: 2),
-        child: UtilButtonIcon(
-          size: 24,
-          isSelected: appData.toolSelected == "shape_drawing",
-          onPressed: () {
-            appData.setToolSelected("shape_drawing");
-          },
-          child: Opacity(
-            opacity: appData.toolSelected == "shape_drawing" ? 1.0 : 0.5,
-            child: Image.asset('assets/images/shape_drawing.png',
-                color: appData.toolSelected == "shape_drawing"
-                    ? colorSelected
-                    : colorUnselected,
-                width: 18,
-                height: 18),
-          ),
-        ),
-      ),
-    ]);
+    return Column(
+      children: tools.map((tool) {
+        Color iconColor = theme.isLight
+            ? appData.toolSelected == tool
+                ? theme.accent
+                : CDKTheme.grey800
+            : appData.toolSelected == tool
+                ? CDKTheme.white
+                : CDKTheme.grey;
+
+        return Container(
+          padding: const EdgeInsets.only(top: 2, left: 2),
+          child: UtilButtonIcon(
+              size: 24,
+              isSelected: appData.toolSelected == tool,
+              onPressed: () {
+                appData.setToolSelected(tool);
+              },
+              child: SvgPicture.asset('assets/images/$tool.svg',
+                  colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn))),
+        );
+      }).toList(),
+    );
   }
 }
